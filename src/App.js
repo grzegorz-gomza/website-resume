a448b7b4504fc3c368ffd543489fc6c9c9411de3
grzegorz-gomza <gomza.datascience@gmail.com>
2025-01-26 14:54:18 +0100

    Some changes in app.js and package.json

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
-import NavBar from './NavBar';
-import Home from './Home';
-import Projects from './Projects';
-import Blogs from './Blogs';
-import Skills from './Skills';
-import Certificates from './Certificates';
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills/Skills';
+import Blogs from './pages/Blogs';
+import Certificates from './pages/Certificates';
 import './App.css';
 
 function App() {
     return (
-        <Box sx={{  minHeight: '100vh'}}>
+        <Box>
             <Router>
-                <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Box>
                     <NavBar />
                 </Box>
-                <Container sx={{ flex: 1, py: 3 }}>
+                <Container>
                     <Routes>
-                        <Route path="/portfolio" exact element={<Home />}/>
+                        <Route path="/" element={<Navigate to="/portfolio" />} />
+                        <Route path="/portfolio" exact element={<Home />} />
                         <Route path="/projects" exact element={<Projects />} />
                         <Route path="/blogs" exact element={<Blogs />} />
                         <Route path="/skills" element={<Skills />} />
                         <Route path="/certificates" element={<Certificates />} />
-                        <Route path="*" element={<Navigate to="/portfolio" replace />} />
                     </Routes>
                 </Container>
             </Router>
         </Box>
     );
 }
+
+export default App;
 
 export default App;
