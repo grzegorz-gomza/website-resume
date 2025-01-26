9f49ecbe29c9f146c21b0c1a2ccf7fe35c30a4ef
grzegorz-gomza <gomza.datascience@gmail.com>
2025-01-26 15:04:06 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home/Home';
