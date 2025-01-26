1567d919e263d551691b5691fd96075a7b62fdcb
grzegorz-gomza <gomza.datascience@gmail.com>
2025-01-26 14:34:38 +0100

    Updates

---
+++ precache-manifest.a1c889e2c5dd08a551ec10028c393fca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaef6186a6055a06f85eda706890e3ea",
+    "url": "/website-resume/index.html"
+  },
+  {
+    "revision": "05a378fae436723a639f",
+    "url": "/website-resume/static/css/main.1702147b.chunk.css"
+  },
+  {
+    "revision": "39b3a10b2950f3b9074f",
+    "url": "/website-resume/static/js/2.10e9316b.chunk.js"
+  },
+  {
+    "revision": "05a378fae436723a639f",
+    "url": "/website-resume/static/js/main.8ceff563.chunk.js"
+  },
+  {
+    "revision": "8e9714e4a32c6de97720",
+    "url": "/website-resume/static/js/runtime~main.139d6d47.js"
+  },
+  {
+    "revision": "ef6facc0ca15fb66db6f7827ff49c9e5",
+    "url": "/website-resume/static/media/GG.ef6facc0.jpg"
+  },
+  {
+    "revision": "80d5745447396df22af5d4c5c626b086",
+    "url": "/website-resume/static/media/cert1.80d57454.png"
+  },
+  {
+    "revision": "67455363c75817968cd71b35f1389b73",
+    "url": "/website-resume/static/media/cert2.67455363.png"
+  },
+  {
+    "revision": "e419f49efc025ecffa364e5a892beb1d",
+    "url": "/website-resume/static/media/cert3.e419f49e.png"
+  },
+  {
+    "revision": "96364ee377f3ae5dfb9c1f10da57372b",
+    "url": "/website-resume/static/media/cert4.96364ee3.png"
+  },
+  {
+    "revision": "2596f1951e3e8882c7b33dc9c6f9858a",
+    "url": "/website-resume/static/media/cert5.2596f195.png"
+  },
+  {
+    "revision": "ec215516306c4bdbcf78c44cbbb9acea",
+    "url": "/website-resume/static/media/cert6.ec215516.png"
+  },
+  {
+    "revision": "172f0015e136a1434011fb4236d3b26d",
+    "url": "/website-resume/static/media/cert7.172f0015.png"
+  },
+  {
+    "revision": "394f436b94ef2ae2142f8b68969f06d3",
+    "url": "/website-resume/static/media/cert8.394f436b.png"
+  },
+  {
+    "revision": "039529d96f71351cd765cfb2abcdf0be",
+    "url": "/website-resume/static/media/dipl.039529d9.png"
+  },
+  {
+    "revision": "31dd459d52dea8afc8db3ff3757b46c5",
+    "url": "/website-resume/static/media/header.31dd459d.png"
+  },
+  {
+    "revision": "c2a70f8d43ce66fe0325e7f33130a838",
+    "url": "/website-resume/static/media/langgraph.c2a70f8d.png"
+  },
+  {
+    "revision": "1043e24ab7483e97bda22032a0f775bf",
+    "url": "/website-resume/static/media/lego.1043e24a.png"
+  },
+  {
+    "revision": "9ab125eecf6681faec8ae100fd440f97",
+    "url": "/website-resume/static/media/punching.9ab125ee.jpeg"
+  }
+]);
