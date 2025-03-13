e8e53141232dd84b0eb7e1a9d7968c583865d3c8
grzegorz-gomza <gomza.datascience@gmail.com>
2025-03-13 11:34:06 +0100

    Blog post - corrections

--- src/assets/configs/blogConfig.js
+++ src/assets/configs/blogConfig.js
@@ -23,7 +23,7 @@ const blogConfig = [
         links: [
             {
                 name: "article",
-                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-2-3b9407871e9d?sk=a2519471d8733c5992160ed5dc76871c",
+                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-1-a5ffa101cf1d?sk=d5e37059ecd224bedd505d9b86370b1a",
                 icon: <SiMedium/>
             }
         ],
@@ -37,11 +37,11 @@ const blogConfig = [
         links: [
             {
                 name: "article",
-                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-2-3b9407871e9d?sk=a2519471d8733c5992160ed5dc76871c",
                 icon: <SiMedium/>
             }
         ],
-        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
         description: "Implementing Vector Search, Relationship-Based Recommendations and other tools in Neo4j & LangChain",
         target: "_blank"
     },
@@ -51,11 +51,11 @@ const blogConfig = [
         links: [
             {
                 name: "article",
-                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-3-a4c02f4eb805?sk=466c637bcba62cc2d4052fd8fd6682c3",
                 icon: <SiMedium/>
             }
         ],
-        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
         description: "Creating the Chat Agents and wrapping up the backend developement.",
         target: "_blank"
     },
@@ -65,11 +65,11 @@ const blogConfig = [
         links: [
             {
                 name: "article",
-                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-4-3798a462bb76?sk=8d048ca3c9891497b54a980b5caa1f3b",
                 icon: <SiMedium/>
             }
         ],
-        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
         description: "Deploying AI-Powered Movie Recommender with Streamlit, Langchain and Neo4J",
         target: "_blank"
     }
