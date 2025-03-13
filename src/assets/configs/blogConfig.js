ce96064a246cf8dfb33a7c508e1b93489dd3c9fa
grzegorz-gomza <gomza.datascience@gmail.com>
2025-03-13 11:35:00 +0100

    Blog corrections

--- src/assets/configs/blogConfig.js
+++ src/assets/configs/blogConfig.js
@@ -4,20 +4,6 @@ import {SiMedium} from "react-icons/si";
 
 const blogConfig = [
     {
-        id: "blog-0",
-        title: "Training a CNN for LEGO Bricks Recognition",
-        links: [
-            {
-                name: "article",
-                url: "https://medium.com/rocket-science-team/training-a-cnn-for-lego-bricks-recognition-f285ffab3327",
-                icon: <SiMedium/>
-            }
-        ],
-        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
-        description: "Building an Convolutional Neural Network to recognise the Lego Bricks, and classify 20 different Bricks.",
-        target: "_blank"
-    },
-    {
         id: "blog-1",
         title: "Building an AI Agent for Movie Recommendations with Neo4j and LangChain (Part 1)",
         links: [
@@ -72,7 +58,21 @@ const blogConfig = [
         image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
         description: "Deploying AI-Powered Movie Recommender with Streamlit, Langchain and Neo4J",
         target: "_blank"
-    }
+    },
+    {
+        id: "blog-0",
+        title: "Training a CNN for LEGO Bricks Recognition",
+        links: [
+            {
+                name: "article",
+                url: "https://medium.com/rocket-science-team/training-a-cnn-for-lego-bricks-recognition-f285ffab3327",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        description: "Building an Convolutional Neural Network to recognise the Lego Bricks, and classify 20 different Bricks.",
+        target: "_blank"
+    },
 ]
 
 export default blogConfig
