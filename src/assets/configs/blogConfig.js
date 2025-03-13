41ead6a9baa3e67b257fa7d23dd36070e902463c
grzegorz-gomza <gomza.datascience@gmail.com>
2025-03-13 11:25:53 +0100

    Added Article to Blog about Netflix Recommender from Medium

--- src/assets/configs/blogConfig.js
+++ src/assets/configs/blogConfig.js
@@ -14,7 +14,63 @@ const blogConfig = [
             }
         ],
         image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
-        description: "Learn how to find the optimal number of positions needed to manage incoming traffic.",
+        description: "Building an Convolutional Neural Network to recognise the Lego Bricks, and classify 20 different Bricks.",
+        target: "_blank"
+    },
+    {
+        id: "blog-1",
+        title: "Building an AI Agent for Movie Recommendations with Neo4j and LangChain (Part 1)",
+        links: [
+            {
+                name: "article",
+                url: "https://medium.com/@gomza.datascience/building-an-ai-agent-for-movie-recommendations-with-neo4j-and-langchain-part-2-3b9407871e9d?sk=a2519471d8733c5992160ed5dc76871c",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+        description: "Setting Up the Neo4j Graph Database for a Netflix Recommendation Chatbot.",
+        target: "_blank"
+    },
+    {
+        id: "blog-2",
+        title: "Building an AI Agent for Movie Recommendations with Neo4j and LangChain (Part 2)",
+        links: [
+            {
+                name: "article",
+                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        description: "Implementing Vector Search, Relationship-Based Recommendations and other tools in Neo4j & LangChain",
+        target: "_blank"
+    },
+    {
+        id: "blog-3",
+        title: "Building an AI Agent for Movie Recommendations with Neo4j and LangChain (Part 3)",
+        links: [
+            {
+                name: "article",
+                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        description: "Creating the Chat Agents and wrapping up the backend developement.",
+        target: "_blank"
+    },
+    {
+        id: "blog-4",
+        title: "Building an AI Agent for Movie Recommendations with Neo4j and LangChain (Part 4)",
+        links: [
+            {
+                name: "article",
+                url: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*BvtLfuY94mGhW2Zg2wjEYA.png",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KyMMz_RUig6hRClOKaOKKg.png",
+        description: "Deploying AI-Powered Movie Recommender with Streamlit, Langchain and Neo4J",
         target: "_blank"
     }
 ]
