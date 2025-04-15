10a25b918d858ba9853d5c3c2c74ee9930d7c53b
grzegorz-gomza <gomza.datascience@gmail.com>
2025-04-15 12:27:05 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website-resume/precache-manifest.aeda318a8c735743a59ed2ccc0d6cf02.js"
+  "/website-resume/precache-manifest.8cd77971000bdaf10112744eea7c8a9c.js"
 );
 
 self.addEventListener('message', (event) => {
